16fa32fabfc64947bffca8e6274ecdd6b77076b8
Emily Carlsen <emily.carlsen@redpillanalytics.com>
2016-12-15 17:20:00 -0500

    Add express server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var express = require('express')();
+var http = require('http').Server(express);
+
+express.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
